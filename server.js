25ad73cc307239c956921ca9baa6c58a62b24bb6
Danny Cairns <dpcairns@gmail.com>
2020-03-17 16:54:05 -0700

    add feedback

--- server.js
+++ server.js
@@ -170,11 +170,7 @@ const getSchemeColors = async() => {
     const colorSchemeData = await request.get(URL);
 
     return colorSchemeData.body.colors.map(color => {
-        return {
-            r: color.rgb.r,
-            g: color.rgb.g,
-            b: color.rgb.b
-        };
+        return color.rgb;
     });
 };
 
@@ -203,11 +199,7 @@ const getClosestColors = async(req) => {
     const colorSchemeData = await request.get(URL);
 
     return colorSchemeData.body.colors.map(color => {
-        return {
-            r: color.rgb.r,
-            g: color.rgb.g,
-            b: color.rgb.b
-        };
+        return color.rgb;
     });
 };
 
